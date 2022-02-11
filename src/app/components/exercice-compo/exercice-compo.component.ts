0f2cfc8fe0587f20cfd80ed40cf3d4c3f4ccb65d
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-02-11 16:52:38 +0100

    Fin exercice
    +vérification lettres
    +clear()
    +boutons

--- src/app/components/exercice-compo/exercice-compo.component.ts
+++ src/app/components/exercice-compo/exercice-compo.component.ts
@@ -10,7 +10,7 @@ export class ExerciceCompoComponent implements OnInit {
   nom: string = "";
   nbrValide: number = 0;
   tentValidation: number = 0;
-  lastNomValide: string = "";
+  lastNomValide ?: string;
 
   constructor() {}
 
@@ -19,23 +19,28 @@ export class ExerciceCompoComponent implements OnInit {
 
   onClick(){
 
-    if(this.nom === 'string') {
-      if(this.nom.length >= 4 && this.nom.length <= 10){
+    let min = 4;
+    let max = 10;
+    let regex = "^[a-zA-Z]{"+min+","+max+"}$";
+
+      if(this.nom.length >= 4 && this.nom.length <= 10 && this.nom.match(regex)){
       console.log("Nom validé !");
-      this. nbrValide = this.nbrValide +1;
+      this.nbrValide ++;
       this.lastNomValide = this.nom;
       } else{
-        console.log("Nom invalide");
+        console.log("Nom invalide: veuillez encoder le nombre valide de lettres !");
       }
-    } else {
-      console.log("Veuillez encoder des lettres !")
-    }
     
-    this.tentValidation = this.tentValidation + 1;
+    this.tentValidation ++;
   }
 
   clear(){
-
+    if(this.tentValidation != 0 || this.nbrValide != 0){
+      this.nom = "";
+      this.lastNomValide = "";
+      this.tentValidation = 0;
+      this.nbrValide = 0;
+    }
   }
 
 }
