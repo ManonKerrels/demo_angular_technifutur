cdb3183a9ae29412e13f773b307c40905d6353db
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-02-14 09:19:45 +0100

    Routing
    +définition routes
    +single page application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemoBindingComponent } from './components/demo-binding/demo-binding.component';
+import { ExerciceCompoComponent } from './components/exercice-compo/exercice-compo.component';
 
-const routes: Routes = [];
+// Définition des routes à utiliser pour avoir telle ou telle page
+const routes: Routes = [
+  { path:"demo", component:DemoBindingComponent },
+  { path:"exo", component:ExerciceCompoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
