c3ec8c1e92c2753fad582d8f8d22b57f88644a92
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2022-02-16 11:25:44 +0100

    Exo Produit
    +fin partie 1

--- src/app/components/exo/exo-produit/exo-produit.component.ts
+++ src/app/components/exo/exo-produit/exo-produit.component.ts
@@ -10,19 +10,16 @@ export class ExoProduitComponent implements OnInit {
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
-
-  en_vente: boolean = false;
-  select: string = "Produit 1";
   
 
   tabProduit: Produit[] = [
-    { id: 1, marque: "Café", modele: "La Caféière", stock: 20, prix: 3, en_vente: true},
+    { id: 1, marque: "Café", modele: "La Caféière", stock: 20, prix: 3, en_vente: true },
     { id: 2, marque: "Thé", modele: "Vert", stock: 30, prix: 5, en_vente: true },
     { id: 3, marque: "Thé", modele: "Noir", stock: 0, prix: 5, en_vente: false }
   ] 
 
   enVente(i: number){
-    this.en_vente = !this.en_vente;
+    this.tabProduit[i].en_vente = !this.tabProduit[i].en_vente;
   }
 
 
