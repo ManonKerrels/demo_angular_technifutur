877689f4a34cb862f27df30e52a229ac76d7f260
ManonKerrels <manon.kerrels@gmail.com>
2022-02-21 16:05:52 +0100

    Bouton valider
    +valid()
    +vérification choix valide ou non vide
    +solution provisoire

--- src/app/components/exo/tp2/tp2.component.ts
+++ src/app/components/exo/tp2/tp2.component.ts
@@ -17,11 +17,17 @@ export class Tp2Component implements OnInit {
     "Pink"
   ]
 
-  color1: string = "white";
-  color2: string = "white";
-  color3: string = "white";
-  color4: string = "white";
-  soluce: boolean = false ;
+  color1: string = "White";
+  color2: string = "White";
+  color3: string = "White";
+  color4: string = "White";
+
+  soluce1: string = "Green";
+  soluce2: string = "Green";
+  soluce3: string = "Green";
+  soluce4: string = "Green";
+
+  oubli: string = "White";
 
   constructor() { }
 
@@ -29,20 +35,26 @@ export class Tp2Component implements OnInit {
   }
 
   reset(){
-    if(this.color1 != "white" || this.color2 != "white" || this.color3 != "white" || this.color4 != "white")
-    this.color1 = "white";
-    this.color2 = "white";
-    this.color3 = "white";
-    this.color4 = "white";
+    if(this.color1 != "White" || this.color2 != "White" || this.color3 != "White" || this.color4 != "White")
+    this.color1 = "White";
+    this.color2 = "White";
+    this.color3 = "White";
+    this.color4 = "White";
   }
 
-  valid(){
-    this.soluce = (this.color1 == "green") && (this.color2 == "green") && (this.color3 == "green") && (this.color4 == "green");
-    if(this.soluce){
-      alert("Bravo, vous avez trouvé la solution !");
-    } else {
-      alert("Dommage ! réessayez");
-    }
+  valid(soluce1: string, soluce2: string, soluce3: string, soluce4: string){
+    soluce1 = "Green";
+    soluce2 = "Green";
+    soluce3  = "Green";
+    soluce4 = "Green";
+
+    if((this.color1 == this.soluce1) && (this.color2 == this.soluce2) && (this.color3 == this.soluce3) && (this.color4 == this.soluce4)){
+        alert("Solution");
+      } else if ((this.color1 == this.oubli) || (this.color2 == this.oubli) || (this.color3 == this.oubli) || (this.color4 == this.oubli)){
+        alert("Oubli");
+      } else {
+        alert("Pas la solution");
+      }
   }
 
 }
