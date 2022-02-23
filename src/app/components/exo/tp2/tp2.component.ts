fad562ccd4cee6701aacb128220741eb1bc9b6fe
ManonKerrels <manon.kerrels@gmail.com>
2022-02-23 12:47:33 +0100

    Méthodes
    +newGame()
    +booléens
    +touched()

--- src/app/components/exo/tp2/tp2.component.ts
+++ src/app/components/exo/tp2/tp2.component.ts
@@ -26,6 +26,7 @@ export class Tp2Component implements OnInit {
   playerWin: boolean = false;
   wrongPlace: boolean = false;
   endGame: boolean = false;
+  inGame: boolean = false;
 
   soluce1: string = "Pink";
   soluce2: string = "Green";
@@ -33,6 +34,7 @@ export class Tp2Component implements OnInit {
   soluce4: string = "Green";
 
   valide: boolean = false;
+  unvalid: boolean = false;
 
 
   oubli: string = "White";
@@ -43,34 +45,52 @@ export class Tp2Component implements OnInit {
   }
 
   reset(){
-    if(this.color1 != "White" || this.color2 != "White" || this.color3 != "White" || this.color4 != "White")
-    this.color1 = "White";
-    this.color2 = "White";
-    this.color3 = "White";
-    this.color4 = "White";
+    if(this.color1 != "White" || this.color2 != "White" || this.color3 != "White" || this.color4 != "White") {
+      this.color1 = "White";
+      this.color2 = "White";
+      this.color3 = "White";
+      this.color4 = "White";
+    }
+    if((10 - this.compteur) != 0){
+      this.compteur = 0;
+    }
+    
   }
 
   valid(){
     if(this.compteur < 10){
       this.valide = true;
+      this.inGame = true;
 
       if((this.color1 == this.soluce1) && (this.color2 == this.soluce2) && (this.color3 == this.soluce3) && (this.color4 == this.soluce4)){
         this.playerWin = true;
       } else if ((this.color1 == this.oubli) || (this.color2 == this.oubli) || (this.color3 == this.oubli) || (this.color4 == this.oubli)){
         alert("Vous avez oublié de remplir une ou plusieurs cases !");
       } else {
+        this.unvalid = true;
         this.compteur ++;
         this.wrongPlace = true;
       }
 
     } else {
+      this.inGame = false;
       this.wrongPlace = false;
       this.endGame = true;
     }
   }
 
   touched(){
+    this.wrongPlace = false;
+    this.endGame = false;
+    this.playerWin = false;
     this.valide = false;
+    this.unvalid = false;
+  }
+
+  newGame(){
+    if((10 - this.compteur) != 0){
+      this.compteur = 0;
+    }
   }
 
 }
