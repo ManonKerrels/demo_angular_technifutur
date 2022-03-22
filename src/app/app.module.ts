7c00f1f4769d368de79c1a8a0f55fdbf488d5c77
ManonKerrels <manon.kerrels@gmail.com>
2022-03-22 09:35:58 +0100

    Nouveau component
    Exo pipe async

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { Page404Component } from './components/page404/page404.component';
+import { PlatAsyncComponent } from './components/exo/menu/plat-async/plat-async.component';
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent,
-    Page404Component
+    Page404Component,
+    PlatAsyncComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/exo/menu/plat-async/plat-async.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plat-async',
+  templateUrl: './plat-async.component.html',
+  styleUrls: ['./plat-async.component.css']
+})
+export class PlatAsyncComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
